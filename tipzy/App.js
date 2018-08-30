eca204bf07ecda0d46551c3e74c9ddd24cab8358
srrana <salman.r.rana@gmail.com>
2018-08-30 10:39:19 -0500

    did not work right connecting computer

--- tipzy/App.js
+++ tipzy/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Main from './src/main';
 
+
+// I hope this works
 export default class App extends React.Component {
   render() {
     return (
